55676d6c170813243bb8d7a65770ab04466eaa64
morisummerz <timofeevnikita111@gmail.com>
2023-05-03 16:26:24 +0300

    Add terms detector and corresponding endpoint

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ from services import detector, glossary
 app = FastAPI()
 
 
-@app.get("/term")
-async def app_get_term_definition(response: Response, word: models.Text):
-    """Returning a definition of provided term"""
-    if not word.text:
+@app.post("/get_terms")
+async def app_get_terms(response: Response, text: models.Text):
+    """Detecting all terms and returning definitions in provided text"""
+    if not text.text:
         response.status_code = 400
         return {"status": "error", "message": "No text provided"}
-    definition = glossary.get_term_definition(word.text)
+    terms = glossary.get_terms(text.text)
+    return {"status": "ok", "terms": terms}
+
+
+@app.get("/term/{word}")
+async def app_get_term_definition(response: Response, word: str):
+    """Returning a definition of provided term"""
+    if not word:
+        response.status_code = 400
+        return {"status": "error", "message": "No text provided"}
+    definition = glossary.get_term_definition(word)
     if definition is None:
         response.status_code = 404
         return {'status': "error", "message": "Provided term not found"}
--- services/glossary.py
+++ services/glossary.py
@@ -1,12 +1,30 @@
 import json
+import re
+import string
 import typing
 from pathlib import Path
+import pymorphy2
 
 glossary = json.loads((Path(__file__).parent / "glossary.json").read_text(encoding="utf-8"))
+morph = pymorphy2.MorphAnalyzer(lang='ru')
 
 
 def get_term_definition(term: str) -> typing.Optional[str]:
-    return glossary.get(term.capitalize().strip(), None)
+    definition = glossary.get(term.capitalize().strip(), None)
+    if definition is None:
+        return glossary.get(morph.parse(term)[0].normal_form.capitalize().strip(), None)
+
+
+def get_terms(text: str) -> typing.Optional[list[dict]]:
+    """Returning a list of terms found in provided text"""
+    result = []
+    for word in text.split():
+        word = re.sub(re.escape(string.punctuation), "", word)
+        if definition := get_term_definition(word):
+            result.append({
+                word: definition
+            })
+    return result
 
 
 if __name__ == '__main__':
