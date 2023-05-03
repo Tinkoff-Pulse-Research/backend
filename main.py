7d24e1b1131aafdfe7e5b83c18c3ae8b1323d274
morisummerz <timofeevnikita111@gmail.com>
2023-05-03 15:44:24 +0300

    Update /detect_slang endpoint

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import random
 
 import models
+from services import detector
 
 app = FastAPI()
 
 
 @app.post("/detect_slang")
-async def app_detect_slang(text: models.Text):
-    is_slang = random.choice([True, False])
+async def app_detect_slang(response: Response, text: models.Text):
+    if not text.text:
+        response.status_code = 400
+        return {"status": "error", "message": "No text provided"}
+    is_slang = detector.detect_slang(text.text)
     return {"status": "ok", "result": is_slang}
 
 
