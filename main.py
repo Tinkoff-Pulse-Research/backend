4363bed821739998b88352191990c2dceda77299
morisummerz <timofeevnikita111@gmail.com>
2023-05-03 15:33:24 +0300

    Add models

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+import random
+
+import models
 
 app = FastAPI()
+
+
+@app.post("/detect_slang")
+async def app_detect_slang(text: models.Text):
+
+    is_slang = random.choice([True, False])
+    return {"status": "ok", "result": is_slang}
 
 
 @app.get("/")
